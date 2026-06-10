c76fb67b6eee7a09cd26a673c693114b684132e8
Emmanuel roshan thomas <emmanuelroshan12@gmail.com>
2026-06-10 08:09:42 +0530

    Update generate keys.py

--- generate keys.py
+++ generate keys.py
@@ -1,1 +1,7 @@
- 
+ from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+# Generate RSA 2048-bit private key
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
